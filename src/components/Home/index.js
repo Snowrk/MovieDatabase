ff0af60cf00c8ac1a401877e2a3a0f2c491c7cf0
Snowrk <shinsan555555@gmail.com>
2024-05-11 14:15:18 +0530

    ntg

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,27 +1,35 @@
-import {useEffect, useState} from 'react'
-import {Audio} from 'react-loader-spinner'
-import CardList from '../CardList'
-import apiKey from '../api'
-import './index.css'
+import { useEffect, useState } from "react";
+import { Audio } from "react-loader-spinner";
+import CardList from "../CardList";
+import apiKey from "../api";
+import "./index.css";
 
 const Home = () => {
-  const [page, setPage] = useState(1)
-  const [isLoading, setIsLoading] = useState(true)
-  const [pageData, setPageData] = useState({})
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pageData, setPageData] = useState({});
 
   useEffect(() => {
     const getData = async () => {
-      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`
-      const response = await fetch(url)
-      const data = await response.json()
-      setPageData(data)
-      setIsLoading(false)
-      console.log(data)
-    }
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setPageData(data);
+      setIsLoading(false);
+      console.log(data);
+    };
 
-    getData()
-  }, [page])
-  return <>{isLoading ? <Audio /> : <CardList pageData={pageData} setPage={setPage} />}</>
-}
+    getData();
+  }, [page]);
+  return (
+    <>
+      {isLoading ? (
+        <Audio />
+      ) : (
+        <CardList pageData={pageData} setPage={setPage} />
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
