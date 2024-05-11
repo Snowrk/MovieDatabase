dd59368144645b0f8fbffc36b2a216c3d2ac9d57
Snowrk <shinsan555555@gmail.com>
2024-05-11 14:52:45 +0530

    'adding suspense'

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,30 +1,41 @@
-import {useEffect, useState} from 'react'
-import { useSearchParams } from 'next/navigation'
-import {Audio} from 'react-loader-spinner'
-import CardList from '../CardList'
-import apiKey from '../api'
-import './index.css'
+import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
+import { Audio } from "react-loader-spinner";
+import CardList from "../CardList";
+import apiKey from "../api";
+import "./index.css";
 
 const Search = () => {
-  const searchParams = useSearchParams()
-  const query = searchParams.get('query')
-  const [page, setPage] = useState(1)
-  const [isLoading, setIsLoading] = useState(true)
-  const [pageData, setPageData] = useState({})
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query");
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pageData, setPageData] = useState({});
 
   useEffect(() => {
     const getData = async () => {
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=${page}`
-      const response = await fetch(url)
-      const data = await response.json()
-      setPageData(data)
-      setIsLoading(false)
-      console.log(data)
-    }
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=${page}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setPageData(data);
+      setIsLoading(false);
+      console.log(data);
+    };
 
-    getData()
-  }, [page])
-  return <>{isLoading ? <Audio /> : <CardList pageData={pageData} setPage={setPage} />}</>
-}
+    getData();
+  }, [page]);
+  return (
+    <>
+      {isLoading ? (
+        <Audio />
+      ) : (
+        <Suspense>
+          <CardList pageData={pageData} setPage={setPage} />
+        </Suspense>
+      )}
+    </>
+  );
+};
 
-export default Search
+export default Search;
